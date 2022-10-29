2bf7fa5b280d9a7d19aba9e995eb367802a384b0
Lewis Jones <lewis@banked.com>
2022-10-29 20:21:15 +0100

    Dictionary run via an Agent

--- dictionary/lib/impl/word_list.ex
+++ dictionary/lib/impl/word_list.ex
@@ -1,17 +1,15 @@
 defmodule Dictionary.Impl.WordList do
-
   @type t :: list(String)
-
 
   @spec word_list() :: t
   def word_list do
-   "../../assets/words.txt"
-   |> Path.expand(__DIR__)
-   |> File.read!()
-   |> String.split(~r/\n/, trim: true)
+    "../../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/\n/, trim: true)
   end
 
-  @spec random_word(t) :: String.t
+  @spec random_word(t) :: String.t()
   def random_word(word_list) do
     word_list
     |> Enum.random()
